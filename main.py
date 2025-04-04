dcee42330a9e1573094197c52733997712d20971
Tim Betz <tim.betz@man-es.com>
2025-04-04 16:11:20 +0200

    raylib + python init

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def main():
-    with open("text/alice_in_wonderland.txt", "r", encoding="utf8") as f:
-        text = f.read()
-    print(f"number words: {len(text.split()):,}")
+from pyray import (
+    VIOLET,
+    WHITE,
+    begin_drawing,
+    clear_background,
+    close_window,
+    draw_text,
+    end_drawing,
+    init_window,
+    window_should_close,
+)
 
-
-if __name__ == "__main__":
-    main()
+init_window(800, 450, "Hello")
+while not window_should_close():
+    begin_drawing()
+    clear_background(WHITE)
+    draw_text("Hello world", 190, 200, 20, VIOLET)
+    end_drawing()
+close_window()
